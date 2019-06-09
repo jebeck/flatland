9b1b61717b7c13a4d05c6bd46490817f5e50f465
Jana Beck <jana.beck@stitchfix.com>
2019-06-09 14:38:11 -0400

    more intro slides (Stitch Fix team), but not done yet

---
+++ components/TeamOrg.js
@@ -0,0 +1,85 @@
+import React from "react"
+
+export default function TeamOrg() {
+  return (
+    <>
+      <h1>(the team)</h1>
+      <div style={{ display: "flex", height: "33vh", width: "80vw" }}>
+        <div
+          style={{
+            alignItems: "center",
+            backgroundColor: "#4ecec9",
+            borderRadius: "0.5rem",
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            margin: "1rem",
+            width: "25%",
+          }}
+        >
+          <h1 style={{ margin: "1rem" }}>A</h1>
+          <h2 style={{ margin: "1rem" }}>client</h2>
+        </div>
+        <div
+          style={{
+            alignItems: "center",
+            backgroundColor: "#4ecec9",
+            borderRadius: "0.5rem",
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            margin: "1rem",
+            width: "25%",
+          }}
+        >
+          <h1 style={{ margin: "1rem" }}>B</h1>
+          <h2 style={{ margin: "1rem" }}>styling</h2>
+        </div>
+        <div
+          style={{
+            alignItems: "center",
+            backgroundColor: "#4ecec9",
+            borderRadius: "0.5rem",
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            margin: "1rem",
+            width: "25%",
+          }}
+        >
+          <h1 style={{ margin: "1rem" }}>C</h1>
+          <h2 style={{ margin: "1rem" }}>merch</h2>
+        </div>
+        <div
+          style={{
+            alignItems: "center",
+            backgroundColor: "#4ecec9",
+            borderRadius: "0.5rem",
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            margin: "1rem",
+            width: "25%",
+          }}
+        >
+          <h1 style={{ margin: "1rem" }}>D</h1>
+          <h2 style={{ margin: "1rem" }}>etc.</h2>
+        </div>
+      </div>
+      <div
+        style={{
+          backgroundColor: "#4ecec9",
+          borderRadius: "0.5rem",
+          display: "flex",
+          flexDirection: "column",
+          height: "20vh",
+          justifyContent: "center",
+          margin: "0 auto",
+          width: "calc(80vw - 2rem)",
+        }}
+      >
+        <h2>data platform</h2>
+      </div>
+    </>
+  )
+}
