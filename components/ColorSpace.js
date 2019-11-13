8a813c3c652be2c8fb2364bfbf275aa17800771b
Jana Beck <jana.beck@stitchfix.com>
2019-11-12 19:22:45 -0800

    bunch o' changes - reuse sphereBufferGeometry for massive initial render speed boost - center color space - add mouse camera controls

--- components/ColorSpace.js
+++ components/ColorSpace.js
@@ -1,14 +1,15 @@
-import React, { useEffect, useMemo, useRef } from "react";
+import React, { useEffect, useMemo, useRef, useState } from "react";
 import PropTypes from "prop-types";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 import { range } from "d3-array";
-import { useThree } from "react-three-fiber";
+import { extend, useRender, useResource, useThree } from "react-three-fiber";
 
 import { getRandomInt } from "../utils";
 
 const CEIL_RGB = 256;
 
 const baseCameraPosition = {
-  rgb: [CEIL_RGB * 1.33333, CEIL_RGB * 1.33333, CEIL_RGB * 1.33333],
+  rgb: [CEIL_RGB, CEIL_RGB, CEIL_RGB],
 };
 
 const baseFocalPoint = {
@@ -34,25 +35,38 @@ const getRandomColors = {
     }),
 };
 
-function ColorSphere({ color, position }) {
-  const ref = useRef();
+/** get position of color with the center of the color space at 0,0,0 instead of a vertex */
+const getPosition = {
+  rgb: (...coords) => coords.map(d => d - CEIL_RGB / 2),
+};
 
+function ColorSphere({ color, geometry, position }) {
   return (
-    <mesh position={position} ref={ref}>
-      <sphereBufferGeometry attach="geometry" args={[3, 25, 25]} />
-      <meshLambertMaterial
-        attach="material"
-        color={color}
-        emissive={color}
-        emissiveIntensity={0.25}
-      />
+    <mesh geometry={geometry} position={position}>
+      <meshLambertMaterial attach="material" color={color} emissive={color} />
     </mesh>
   );
 }
 
-export default function ColorSpace({ type, n = 10000 }) {
-  const data = useMemo(() => getRandomColors[type](n), [n, type]);
+extend({ OrbitControls });
 
+function CameraControls(props) {
+  const { camera } = useThree();
+  const controls = useRef();
+
+  const [canvasEl] = useState(document.getElementsByTagName("canvas")[0]);
+
+  useRender(() => controls.current && controls.current.update());
+
+  return <orbitControls args={[camera, canvasEl]} ref={controls} {...props} />;
+}
+
+export default function ColorSpace({
+  type,
+  n = Math.pow(Math.floor(CEIL_RGB / 20), 3),
+}) {
+  const data = useMemo(() => getRandomColors[type](n), [n, type]);
+  const [geometryRef, geometry] = useResource();
   const { camera } = useThree();
 
   useEffect(() => {
@@ -62,12 +76,15 @@ export default function ColorSpace({ type, n = 10000 }) {
 
   return (
     <>
+      <CameraControls />
+      <sphereBufferGeometry args={[2.5, 25, 25]} ref={geometryRef} />
       <spotLight position={baseCameraPosition[type]} />
       {data.map((d, i) => (
         <ColorSphere
           color={d.color}
+          geometry={geometry}
           key={`${i}-${d.color}`}
-          position={[d.r, d.g, d.b]}
+          position={getPosition[type](d.r, d.g, d.b)}
         />
       ))}
     </>
