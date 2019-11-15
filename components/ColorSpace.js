8a5be959f862ee952223d4a483679bc7df0fd64f
Jana Beck <jana.beck@stitchfix.com>
2019-11-14 18:31:21 -0800

    props and propTypes clean up

--- components/ColorSpace.js
+++ components/ColorSpace.js
@@ -28,6 +28,13 @@ function ColorSphere({ color, geometry, position }) {
   );
 }
 
+ColorSphere.propTypes = {
+  color: PropTypes.string.isRequired,
+  /** because is a ref, will be undefined on first render */
+  geometry: PropTypes.object,
+  position: PropTypes.arrayOf(PropTypes.number).isRequired,
+};
+
 extend({ OrbitControls });
 
 function CameraControls() {
@@ -41,7 +48,7 @@ function CameraControls() {
   return <orbitControls args={[camera, canvasEl]} autoRotate ref={controls} />;
 }
 
-function Spotlight({ type }) {
+function Spotlight() {
   const spotLightRef = useRef();
   const { camera } = useThree();
   useFrame(() => spotLightRef.current.position.copy(camera.position));
@@ -49,37 +56,42 @@ function Spotlight({ type }) {
   return <spotLight position={startingCameraPosition} ref={spotLightRef} />;
 }
 
-export default function ColorSpace({
-  data,
-  sphereRadius = 2,
-  spaceRadius = 50,
-  type,
-}) {
+export default function ColorSpace({ data, sphereRadius, spaceRadius, space }) {
   const [geometryRef, geometry] = useResource();
   const { camera } = useThree();
 
   useEffect(() => {
     camera.position.set(...startingCameraPosition);
     camera.lookAt(...startingFocalPoint);
-  }, [camera, type]);
+  }, [camera, space]);
 
   return (
     <>
       <CameraControls />
-      <Spotlight type={type} />
+      <Spotlight />
       <sphereBufferGeometry args={[sphereRadius, 25, 25]} ref={geometryRef} />
       {data.map((d, i) => (
         <ColorSphere
           color={d.color}
           geometry={geometry}
           key={`${i}-${d.color}`}
-          position={getColorXYZPosition[type](spaceRadius, d)}
+          position={getColorXYZPosition[space](spaceRadius, d)}
         />
       ))}
     </>
   );
 }
 
+ColorSpace.defaultProps = {
+  spaceRadius: 50,
+  sphereRadius: 2,
+};
+
 ColorSpace.propTypes = {
-  type: PropTypes.oneOf(["hsl", "lab", "rgb"]).isRequired,
+  data: PropTypes.arrayOf(
+    PropTypes.shape({ color: PropTypes.string.isRequired })
+  ).isRequired,
+  space: PropTypes.oneOf(["hsl", "lab", "rgb"]).isRequired,
+  spaceRadius: PropTypes.number.isRequired,
+  sphereRadius: PropTypes.number,
 };
