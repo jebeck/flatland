7b2fb3b9d9ddc8e461a6fc660659cb807e07ecc8
Jana Beck <jana.beck@stitchfix.com>
2019-11-20 17:15:58 -0800

    add maps example image + credit component; remove three.js demos for now

---
+++ components/Credit.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+export default function Credit({ asset, author, link }) {
+  return (
+    <div
+      style={{
+        bottom: "1rem",
+        fontSize: "0.875rem",
+        position: "absolute",
+        right: "1rem",
+      }}
+    >
+      {`${asset} by ${author} (`}
+      <a
+        href={link}
+        rel="noopener noreferrer"
+        style={{ color: "#9AB0E5" }}
+        target="_blank"
+      >
+        source
+      </a>
+      )
+    </div>
+  );
+}
